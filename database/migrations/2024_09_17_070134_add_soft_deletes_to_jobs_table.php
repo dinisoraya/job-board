1ef56d11ddb38c63b55eeabfacd1379d537bb1f1
IbnuMuhtar <112595037+IbnuMuhtar@users.noreply.github.com>
2024-11-14 22:06:15 +0700

    soft deletes

--- database/migrations/2024_09_17_070134_add_soft_deletes_to_jobs_table.php
+++ database/migrations/2024_09_17_070134_add_soft_deletes_to_jobs_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration {
     /**
      * Run the migrations.
@@ -14,7 +12,6 @@ return new class extends Migration {
             $table->softDeletes();
         });
     }
-
     /**
      * Reverse the migrations.
      */
